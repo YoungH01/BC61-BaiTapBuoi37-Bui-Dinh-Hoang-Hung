95cd878eba8b92b733f2a4675ab9d951f54e22f2
heung <hoanghung.bkevent@gmail.com>
2024-05-01 10:48:06 +0700

    loading 25%

---
+++ homework37/src/redux/studentReducer.jsx
@@ -0,0 +1,9 @@
+const stateDefault={
+    DanhSachSinhVien:[]
+};
+const studentReducer=(state=stateDefault,action)=>{
+    switch(action.type){
+        default: return {...state};
+    }
+}
+export default studentReducer;
